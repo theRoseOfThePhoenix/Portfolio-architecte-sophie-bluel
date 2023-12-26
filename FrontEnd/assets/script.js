7da665033ccb585fb246fbb76a658a6f433b7616
theRoseOfThePhoenix <sandrinebecaert@gmail.com>
2023-12-26 15:50:39 +0100

    modification du fetch et mise en place des filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,111 +1,93 @@
-/*
-const reponse = await fetch("http://localhost:5678/api/works")
-  .then(response => {
-    // La méthode json() renvoie une Promise qui résout avec le corps du JSON parsé
-    return response.json();
-  })
-  .then(data => {
-    // Faites quelque chose avec les données récupérées
-    const gallery = data
-    console.log(gallery);
-    getAllWork();
-  })
 
-  .catch(error => {
-    // Gérer les erreurs survenues lors de la requête
-    console.error('Erreur de fetch:', error);
-  });*/
 
-export async function getAllWork() {
-    const response = await fetch("http://localhost:5678/api/works");
-    const gallery = await response.json();
-    console.log(gallery)
+let gallerys = await fetch('http://localhost:5678/api/works').then(works => works.json());
 
-    for(let i = 0; i < gallery.length; i++) {
-        
-  // Récupération de l'élément du DOM qui accueillera la gallery
-    const sectionGallery = document.querySelector(".gallery");
-  // Création d’une balise dédiée à une photo
-  const figureElement = document.createElement("figure");
-  const imageElement = document.createElement("img");
-  imageElement.src = gallery[i].imageUrl;
-  imageElement.alt = gallery[i].title;
+console.log(gallerys)
 
- const descriptionPhoto = document.createElement("figcaption");
- descriptionPhoto.textContent = gallery[i].title; 
+function creatGallery(gallerys){
 
- const idElement = document.createElement("id");
- idElement.id =  gallery[i].categoryId;
- console.log(idElement);
- console.log(figureElement);
- 
- // On rattache la balise photo à la section gallery
- sectionGallery.appendChild(figureElement);
- figureElement.appendChild(imageElement);
- figureElement.appendChild(descriptionPhoto);
- figureElement.appendChild(idElement);
+  for(let i = 0; i < gallerys.length; i++) {
+
+      const loop = gallerys[i];      
+      // Récupération de l'élément du DOM qui accueillera la gallery
+      const sectionGallery = document.querySelector(".gallery");
+      // Création d’une balise dédiée à une photo
+      const figureElement = document.createElement("figure");
+      const imageElement = document.createElement("img");
+      imageElement.src = loop.imageUrl;
+      imageElement.alt = loop.title;
+    const descriptionPhoto = document.createElement("figcaption");
+    descriptionPhoto.innerText = loop.title; 
+      const idElement = document.createElement("id");
+    idElement.id =  loop.categoryId;
+    
+    console.log(idElement);
+    console.log(figureElement);
+    
+    // On rattache la balise photo à la section gallery
+    sectionGallery.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(descriptionPhoto);
+    figureElement.appendChild(idElement);
   }
 }
-getAllWork();
+creatGallery(gallerys);
 
+// Le bouton Tous
 const btnAll = document.querySelector(".filterAll");
     btnAll.addEventListener("click", function () {
-      getAllWork();
-      console.log("youyou");
+      //remettre à zero
+      document.querySelector(".gallery").innerText = ""; 
+      creatGallery(gallerys);
+      document.querySelectorAll("#filtres button").forEach(function(btn) {
+        btn.classList.remove("active");
     });
-
+    btnAll.classList.add("active");
     
+      console.log("youyou");
+});
 
-async function objectFilter() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const gallery = await response.json();
+// Bouton Objets    
+  const btnObjet = document.querySelector(".filterObjects");
+  btnObjet.addEventListener("click", function()  {
+    const galleryObjets = gallerys.filter(function(gallerys) {
+        return gallerys.category.id === 1;
+    });
+    document.querySelector(".gallery").innerText = "";
+    creatGallery(galleryObjets);
+    document.querySelectorAll("#filtres button").forEach(function(btn) {
+        btn.classList.remove("active");
+    });
+    btnObjet.classList.add("active");
+    
+});
 
-
-  const boutonObjet = document.querySelector(".filterObjects");
-  boutonObjet.addEventListener("click",  () => {
-    for(let i = 0; i < gallery.length; i++) {
-      const tableauObjet = gallery.filter((id) => id.categoryId == 1 );
-      console.log(tableauObjet);
-      const sectionGallery = document.querySelector(".gallery");
-      sectionGallery.innerHTML = "";
-
-
-      return tableauObjet
-    };
+// Bouton Appartements
+const btnAppartements = document.querySelector(".fitlerApartments");
+btnAppartements.addEventListener ("click",  function() {
+    const galleryAppartements = gallerys.filter(function(gallerys) {
+      return gallerys.category.id === 2;
+    });
+    document.querySelector(".gallery").innerText = "";
+    creatGallery(galleryAppartements);
+    document.querySelectorAll("#filtres button").forEach(function(btn) {
+      btn.classList.remove("active");
   });
-}
-objectFilter()
-
-async function appartementsFilter() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const gallery = await response.json();
-const boutonAppartements = document.querySelector(".fitlerApartments");
-boutonAppartements.addEventListener("click",  () => {
-    for(let i = 0; i < gallery.length; i++) {
-      const tableauAppartements = gallery.filter((id) => id.categoryId == 2 );
-      console.log(tableauAppartements);
-      const sectionGallery = document.querySelector(".gallery");
-      sectionGallery.innerHTML = "";
-      return tableauAppartements
-    };
-  });
-}
-  appartementsFilter()
-
-
-
-async function hotelRetauFilter() {
-  const response = await fetch("http://localhost:5678/api/works");
-  const gallery = await response.json();
+  btnAppartements.classList.add("active");
+    });
+  
+// Bouton Hôtels & Restaurants
 const btnHotelResto = document.querySelector(".filterHotelsRestaurants");
-btnHotelResto.addEventListener("click",  () => {
-    for(let i = 0; i < gallery.length; i++) {
-      const tableaHotelsRestaurants = gallery.filter((id) => id.categoryId == 3 );
-      console.log(tableaHotelsRestaurants);
-      const sectionGallery = document.querySelector(".gallery");
-      sectionGallery.innerHTML = "";
-      return tableaHotelsRestaurants
-    };
-  });
-}
-hotelRetauFilter()
+btnHotelResto.addEventListener("click",  function() {
+      const galleryHotelsRestaurants = gallerys.filter(function(gallerys) {
+        return gallerys.category.id ===3;
+      });
+      document.querySelector(".gallery").innerText = "";
+      creatGallery(galleryHotelsRestaurants);
+      document.querySelectorAll("#filtres button").forEach(function(btn) {
+        btn.classList.remove("active");
+    });
+    btnHotelResto.classList.add("active");
+ });
+      
+    
